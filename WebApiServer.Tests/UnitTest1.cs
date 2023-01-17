a4a140d7139dc157bc1c24420916d4759b722208
Dmitri Shavshin <shavshindm@outlook.com>
2023-01-17 17:08:00 +0200

    Adding xUnit tests to solution

---
+++ WebApiServer.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebApiServer.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- WebApiServer/Services/EmailService.cs
+++ WebApiServer/Services/EmailService.cs
@@ -1,6 +1,32 @@
-﻿namespace WebApiServer.Services
+﻿using Microsoft.Extensions.Configuration;
+using RestSharp.Authenticators;
+using RestSharp;
+
+namespace WebApiServer.Services
 {
     public class EmailService
     {
+        private readonly IConfiguration _configuration;
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration= configuration;
+        }
+
+        private bool SendEmail(string body, string email)
+        {
+            var client = new RestClient("https://api.mailgun.net/v3");
+            var request = new RestRequest("", Method.Post);
+            var API_KEY = _configuration.GetSection("EmailConfig:API_KEY").Value;
+            client.Authenticator = new HttpBasicAuthenticator("api", API_KEY);
+            request.AddParameter("domain", "sandbox6c34104b1465483a85377d7499094915.mailgun.org");
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Online Shop <OnlineShop@mail.com>");
+            request.AddParameter("to", "lineageiivk@gmail.com");
+            request.AddParameter("subject", "Email Verification");
+            request.AddParameter("text", body);
+            request.Method = Method.Post;
+            var response = client.Execute(request);
+            return response.IsSuccessful;
+        }
     }
 }
